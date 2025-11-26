a35becaa7d4f2fe747beeb375eaf3fbfc30f8e38
K-Jamal <302076413@student.rocmondriaan.nl>
2025-11-26 16:05:35 +0100

    added a typing animation and also a style change

---
+++ js/main.js
@@ -0,0 +1,10 @@
+  document.addEventListener('DOMContentLoaded', () => {
+    var options = {
+      strings: ['Software Developer', 'Web Developer', 'Front-end developer', 'Tech Enthusiast'],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true
+    };
+
+    var typed = new Typed('.typed', options);
+  });
