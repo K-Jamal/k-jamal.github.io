b903b1dc5197aa90273747b9decf40a1debeb637
K-Jamal <302076413@student.rocmondriaan.nl>
2025-11-27 15:04:55 +0100

    Added Dark Mode Toggle and social icons removed comments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-  document.addEventListener('DOMContentLoaded', () => {
+console.log("Console Loaded");
+
+document.querySelector('.mode-switch').addEventListener('click',()=>{
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+    }
+})
+
+
+document.addEventListener('DOMContentLoaded', () => {
     var options = {
       strings: ['Software Developer', 'Web Developer', 'Front-end developer', 'Tech Enthusiast'],
       typeSpeed: 50,
