ecce289ba1a5eaecf421e7063dde882efe1b20cb
K-Jamal <302076413@student.rocmondriaan.nl>
2025-11-28 22:42:01 +0100

    changed skills from bars to pie charts

--- js/main.js
+++ js/main.js
@@ -20,3 +20,56 @@ document.addEventListener('DOMContentLoaded', () => {
 
     var typed = new Typed('.typed', options);
   });
+
+
+
+  
+//we halen de canvas op uit het HTML document met de class naam chart-example
+const chart = document.querySelector('.front-end-chart');
+
+//er wordt een nieuwe grafiek gemaakt van het type 'bar'
+new Chart(chart, {
+    type: 'doughnut',
+    data: {
+      //de labels komen op de x as
+      labels: ['HTML/CSS', 'Bootstrap', 'Javascript'],
+      datasets: [{
+        label: ' Lines Written',
+        // de data komt op de y as
+        data: [5000, 2500, 3400],
+        borderWidth: 3
+      }]
+    },
+    options: {
+      scales: {
+        // y: {
+        //   beginAtZero: true
+        // }
+      }
+    }
+});
+
+
+const backChart = document.querySelector('.back-end-chart');
+
+//er wordt een nieuwe grafiek gemaakt van het type 'bar'
+new  Chart(backChart, {
+    type: 'pie',
+    data: {
+      //de labels komen op de x as
+      labels: ['PHP', 'Symfony', 'SQL', "Java"],
+      datasets: [{
+        label: ' Lines Written',
+        // de data komt op de y as
+        data: [5000, 2500, 3400, 2000],
+        borderWidth: 3
+      }]
+    },
+    options: {
+      scales: {
+        // y: {
+        //   beginAtZero: true
+        // }
+      }
+    }
+});
